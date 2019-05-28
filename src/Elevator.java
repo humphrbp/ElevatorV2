0ef9f5682ae734a5f9efba14ed80bc46e304ffb4
humphrbp <humphrbp@users.noreply.github.com>
2019-05-27 23:44:22 -0400

    Initial commit

---
+++ src/Elevator.java
@@ -0,0 +1,26 @@
+
+//****************************************************************//
+ //				TITLE:  ELEVATOR SIMULATOR						 //
+  //			AUTHOR: BENJAMIN HUMPHREY	
+   //           
+    //******************************************************//
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Elevator {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame ("Elevator Simultor");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new ElevatorPanel());
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
+
