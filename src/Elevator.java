252784c17e49c3f234cdadd3de24d69f208e892a
humphrbp <humphrbp@users.noreply.github.com>
2019-05-29 18:58:25 -0400

    Format Edits

--- src/Elevator.java
+++ src/Elevator.java
@@ -2,19 +2,17 @@
 //****************************************************************//
  //				TITLE:  ELEVATOR SIMULATOR						 //
   //			AUTHOR: BENJAMIN HUMPHREY	
-   //           
+   //            
     //******************************************************//
 
 import javax.swing.JFrame;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
 
 public class Elevator {
 
 	public static void main(String[] args) {
 
-		JFrame frame = new JFrame ("Elevator Simultor");
+		JFrame frame = new JFrame ("Elevator Simultor for Luke");  
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new ElevatorPanel());
 
